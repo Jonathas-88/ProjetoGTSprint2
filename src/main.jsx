3ae4dabbb3de994c9bec84ebad31b15bee4105ec
jonathas <jonathas.sabino@gmail.com>
2025-05-19 16:53:37 -0300

    consumo API

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { CartProvider } from './context/CartContext.jsx'
+import { ProductsProvider } from './context/ProductsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ProductsProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+    </ProductsProvider>
   </StrictMode>,
 )
---
+++ src/pages/PageCarrinho.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carrinho from '../components/Carrinho'
+import ProductCard from '../components/ProductCard'
+
+
+const PageCarrinho = () => {
+  return (
+    <>
+      <Carrinho/>
+      <ProductCard/>
+    </>
+  )
+}
+
+export default PageCarrinho
